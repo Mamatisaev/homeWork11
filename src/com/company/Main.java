9c038d6c8c91a84d8cdc1f4a1e127221e0815651
Mamatisaev <umamatisaev@gmail.com>
2022-05-04 05:02:16 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+	triangle.a = 5;
+    triangle.b = 5;
+    triangle.c = 2.5;
+    triangle.area();
+
+        }
+    }
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Triangle {
+    int a;
+    int b;
+    double c;
+    void area() {
+        System.out.println("The area of this triangle is " + (b * c) / 2 + " cm.");
+    }
+}
